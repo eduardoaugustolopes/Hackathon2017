c521bcac1b491cf2905e6aea6cc323defe1dfc0d
Edson Santos <edsonsantana@sommus.com>
2017-07-15 04:28:34 -0300

    .

--- CidadaoAlerta/CidadaoAlerta/www/App/Js/principal.js
+++ CidadaoAlerta/CidadaoAlerta/www/App/Js/principal.js
@@ -8,8 +8,7 @@
                 "</a>"+
                 "<ul class='right hide-on-med-and-down'>"+
                     "<li><a href='#'>Ocorrências</a></li>"+
-<<<<<<< HEAD
-                    "<li><a href='tel:37988174539'>Emergências</a></li>"+
+                    "<li><a href='tel:3732619150'>Emergências</a></li>"+
                     "<li><a href='#'>Polícia Militar</a></li>"+
                     "<li><a href='#'>Polícia Ambiental</a></li>"+
                     "<li><a href='#'>Polícia Cívil</a></li>"+
@@ -20,11 +19,9 @@
                     "<li><a href='#'>Polícia Militar</a></li>" +
                     "<li><a href='#'>Polícia Ambiental</a></li>" +
                     "<li><a href='#'>Polícia Cívil</a></li>" +
-=======
                 "</ul>"+
                 "<ul class='side-nav' id='menu-mobile'>"+
                     "<li><a onclick='criaTelaIncluirDelito();'>Nova Ocorrência</a></li>"+
->>>>>>> 4532944ceded1e5b523ed79bb168cccde68834fa
                 "</ul>"+
             "</div>"+
         "</nav>"+
@@ -100,7 +97,26 @@ function onClickOcorrencia(id) {
 
 function desenhaTelaOcorrencia(ocorrencia) {
     $("#conteudo").html(
-        ""
+        "<div id='exibeOcorrencia'  class='container' style='padding-top: 100px'>" +
+            "<form class='form-horizontal'>" +
+                "<div class='form-group'>" +
+                    "<label class='col-sm-2' for='descricao'>Descrição</label>" +
+                    "<div class='col-sm-10'>" +
+                        "<p class='form-control-static'>" + ocorrencia.descricao + "</p > " +
+                    "</div>" +
+
+                    "<label class='col-sm-2' for='data'>Data</label>" +
+                    "<div class='col-sm-10'>" +
+                        "<p class='form-control-static'>" + ocorrencia.data + "</p>" +
+                    "</div>" +
+
+                    "<label class='col-sm-2' for='tipo'>Tipo de ocorrência</label>" +
+                    "<div class='col-sm-10'>" +
+                        "<p class='form-control-static'>" + ocorrencia.tipo + "</p>" +
+                    "</div>" +
+                "</div>" +
+            "</form> " +
+        "</div>"
     );
 }
 
